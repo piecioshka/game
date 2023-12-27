aaa94815f1efe0b51e7b4a3690ad551744de939b
Piotr Kowalski <piecioshka@gmail.com>
2023-12-27 12:48:12 +0100

    Cleanup utilities

---
+++ src/scripts/engine/utils/storage.js
@@ -0,0 +1,26 @@
+export const Storage = {
+  create(key, val) {
+    try {
+      const value = JSON.stringify(val);
+      localStorage.setItem(key, value);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  read(key) {
+    try {
+      const value = localStorage.getItem(key);
+      return value ? JSON.parse(value) : null;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  },
+  update(key, val) {
+    this.create(key, val);
+  },
+
+  delete(key) {
+    localStorage.removeItem(key);
+  },
+};
