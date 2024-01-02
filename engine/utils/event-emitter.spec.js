dbc3c390779eeba34e6ebd45520f6d15d778f1aa
Piotr Kowalski <piecioshka@gmail.com>
2024-01-02 23:15:47 +0100

    Update UTs

--- engine/utils/event-emitter.spec.js
+++ engine/utils/event-emitter.spec.js
@@ -2,14 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { EventEmitter } from './event-emitter';
 
 describe('EventEmitter', () => {
-  it('should register an event', () => {
-    const ee = new EventEmitter();
-    const cb = () => null;
-    ee.on('test', cb);
-    expect(ee._listeners).toEqual({ test: [{ name: 'test', cb }] });
-  });
-
-  it('should dispatch an event', () => {
+  it('should register & dispatch an event', () => {
     const ee = new EventEmitter();
     let triggered = false;
     const cb = () => {
